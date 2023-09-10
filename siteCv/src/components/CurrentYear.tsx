f632f017230831d689c7259826c6267a7588cfab
Romain GAUTIER <romain.gautier@etu.univ-nantes.fr>
2023-09-10 20:49:03 +0200

    first commit

---
+++ siteCv/src/components/CurrentYear.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function CurrentYear(){
+    return (
+      <div>
+        <h2>Home Page</h2>
+        {/* Your Home content here */}
+      </div>
+    );
+  };
+  
---
+++ siteCv/src/components/Experience.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Experience(){
+    return (
+      <div>
+        <h2>Home Page</h2>
+        {/* Your Home content here */}
+      </div>
+    );
+  };
+  
