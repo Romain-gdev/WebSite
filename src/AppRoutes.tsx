5e4d2ffbfb925e7d04b2a26c9cf326600e14c27f
Romain GAUTIER <romain.gautier@etu.univ-nantes.fr>
2023-09-11 10:17:15 +0200

    hasRouter pour déploiement

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,9 +1,12 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, HashRouter } from "react-router-dom";
 import { Home } from "./components/Home";
+import React from "react";
 
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
+      <HashRouter basename="/" >
         <Route path="/home" element={<Home />}></Route>
+      </HashRouter>
       )
 )
